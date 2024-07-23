61ec73cd425e3872b5b42399ed6550d7a71aa466
AndreevAlexey <andreev-j@yandex.ru>
2024-07-23 19:39:29 +0300

    refactor CommentRepository, method findByBookId

--- hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -2,8 +2,6 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
@@ -21,7 +19,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAll();
 
 
-    @Query("select c from Comment c where c.book.id = :bookId")
-    List<Comment> findByBookId(@Param("bookId") long bookId);
+    List<Comment> findByBookId(long bookId);
 
 }
