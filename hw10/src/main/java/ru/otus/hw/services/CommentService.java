767c67980ec2fd75468ba50794ff4617b2fccd5f
AndreevAlexey <andreev-j@yandex.ru>
2024-08-27 12:05:32 +0300

    add homework10 code

---
+++ hw10/src/main/java/ru/otus/hw/services/CommentService.java
@@ -0,0 +1,20 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+    Optional<Comment> findById(long id);
+
+    List<Comment> findAll();
+
+    List<Comment> findAllByBookId(long bookId);
+
+    Comment insert(Comment comment);
+
+    Comment update(Comment comment);
+
+    void deleteById(long id);
+}
